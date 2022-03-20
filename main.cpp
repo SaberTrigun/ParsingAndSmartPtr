458a66c357c497299718d8b704154edb6129d229
trigun <00700590@tutanota.com>
2022-03-20 23:21:24 +0200

    Create two little project for try OOP and smart pointer

---
+++ main.cpp
@@ -0,0 +1,290 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+
+class CounterCash
+{
+protected:
+    std::string name;
+    unsigned int workTime;
+    unsigned int salary;
+private:
+    unsigned int coeffDirector;
+    unsigned int coeffProgrammer;
+    unsigned int coeffAssistant;
+
+    std::string fileText;
+    std::string clearText;
+
+    unsigned int counterDelimStaff;
+    unsigned int currentCursorPosition;
+    const unsigned int TOTALNUMBERCOLUMN;
+    std::string** tableFileText;
+public:
+    CounterCash() : name(), workTime(0), salary(0), coeffDirector(0), coeffProgrammer(0), coeffAssistant(0), fileText(), clearText(), counterDelimStaff(0), currentCursorPosition(0), TOTALNUMBERCOLUMN(3), tableFileText(nullptr)
+    {}
+
+
+    CounterCash(unsigned int CoeffDir, unsigned int CoeffProg, unsigned int CoeffAssist) :
+        name(),
+        workTime(0),
+        salary(0),
+        coeffDirector(CoeffDir),
+        coeffProgrammer(CoeffProg),
+        coeffAssistant(CoeffAssist),
+        fileText(),
+        clearText(),
+        counterDelimStaff(0),
+        currentCursorPosition(0),
+        TOTALNUMBERCOLUMN(3),
+        tableFileText(nullptr)
+    {}
+
+
+    // Считывание текста из файла без упраляющих символов типа '\n', '\r', EOF и так далее.
+    std::string readFileToStr(const char* filePath)
+    {
+        std::ifstream fileStream(filePath, std::ios::in);
+        if(!fileStream)
+        {
+            std::cerr << "ERROR Open file" << std::endl;
+            exit(1);
+        }
+        while(fileStream)
+        {
+            getline(fileStream, fileText);
+            clearText = clearText + fileText;
+        }
+        return clearText;
+    }
+
+
+    // Считаем общее колличество записей/сотрудников в строке
+    int getTotalNumberEmployees(std::string& clearText)
+    {
+        for(unsigned int i = 0; i < clearText.length() ; ++i)
+        {
+            if(clearText[i] == ';')
+            {
+                ++counterDelimStaff;
+            }
+        }
+        counterDelimStaff = counterDelimStaff / 3;
+
+        return counterDelimStaff;
+    }
+
+
+    // Парсим строку в таблицу |должность|фамилия|время|
+    std::string** parsingString(std::string& clearText)
+    {
+        const unsigned int TOTALNUMBERLINE = counterDelimStaff;
+        tableFileText = new std::string* [TOTALNUMBERLINE];
+        for(int i = 0; i < TOTALNUMBERLINE; ++i)
+        {
+            tableFileText[i] = new std::string [TOTALNUMBERCOLUMN];
+        }
+
+        //Парсим чистую строку без спец символов clearText в таблицу tableFileText
+        for(unsigned int i = 0; i < TOTALNUMBERLINE; ++i)
+        {
+            for(unsigned int j = 0; j < TOTALNUMBERCOLUMN; ++j) // 3
+            {
+                while(clearText[currentCursorPosition] != ';')
+                {
+                    tableFileText[i][j] = tableFileText[i][j] + clearText[currentCursorPosition];
+                    ++currentCursorPosition;
+                }
+                ++currentCursorPosition;
+            }
+        }
+        return tableFileText;
+    }
+
+
+    virtual void payRoll(CounterCash& counterCash)
+    {}
+
+
+    virtual void showSalary()
+    {}
+
+
+    int crutchDir ()
+    {return coeffDirector;}
+
+
+    int crutchProg ()
+    {return coeffProgrammer;}
+
+
+    int crutchAssist ()
+    {return coeffAssistant;}
+
+
+    virtual ~CounterCash()
+    {
+        for(int i = 0; i < counterDelimStaff; ++i)
+        {
+            delete [] tableFileText[i];
+        }
+        delete [] tableFileText;
+    }
+};
+
+
+class Director : public CounterCash
+{
+public:
+    Director(std::string& Name, int WorkTime)
+    {
+        name = Name;
+        workTime = WorkTime;
+    }
+
+
+    void payRoll(CounterCash& counterCash)
+    {
+        unsigned int crutchCoeff = counterCash.crutchDir();
+        salary = workTime * crutchCoeff;
+    }
+
+
+    void showSalary()
+    {
+        std::string str = std::to_string(salary);
+        std::cout << name << "  " << str + "$" << std::endl;
+    }
+
+
+    ~Director()
+    {}
+};
+
+
+class Programmer : public CounterCash
+{
+public:
+    Programmer(std::string& Name, int WorkTime)
+    {
+        name = Name;
+        workTime = WorkTime;
+    }
+
+    void payRoll(CounterCash& counterCash)
+    {
+        unsigned int crutchCoeff = counterCash.crutchProg();
+        salary = workTime * crutchCoeff;
+    }
+
+    void showSalary()
+    {
+        std::string str = std::to_string(salary);
+        std::cout << name << "  " << str + "$" << std::endl;
+    }
+
+    ~Programmer()
+    {}
+};
+
+
+class Assistant : public CounterCash
+{
+public:
+    Assistant(std::string& Name, int WorkTime)
+    {
+        name = Name;
+        workTime = WorkTime;
+    }
+
+    void payRoll(CounterCash& counterCash)
+    {
+        unsigned int crutchCoeff = counterCash.crutchAssist();
+        salary = workTime * crutchCoeff;
+    }
+
+    void showSalary()
+    {
+        std::string str = std::to_string(salary);
+        std::cout << name << "  " << str + "$" << std::endl;
+    }
+
+    ~Assistant()
+    {}
+};
+
+
+void salaryCalculation(int CounterDelimStaff, std::string& EntryPosition, CounterCash& counterCash, std::string** TableFileText, std::vector<CounterCash*>& PtrCounterCash)
+{
+    unsigned int convertStringToIntPay = 0;
+    unsigned int index = 0;
+
+    for (int i = 0; i < CounterDelimStaff; ++i)
+    {
+        if(TableFileText[i][0] == EntryPosition)
+        {
+            std::istringstream(TableFileText[i][2]) >> convertStringToIntPay;
+            PtrCounterCash.insert(PtrCounterCash.end(), new Director(TableFileText[i][1], convertStringToIntPay));
+            PtrCounterCash[index]->payRoll(counterCash);
+            PtrCounterCash[index]->showSalary();
+            ++index;
+        }
+    }
+
+    if(index != 0)
+    {
+        for(int i = 0; i < index; ++i)
+        {
+            delete PtrCounterCash[i];
+        }
+        index = 0;
+        PtrCounterCash.clear();
+    }
+}
+
+
+int main()
+{
+    CounterCash counterCash(500, 350, 200);
+    std::string textFile = counterCash.readFileToStr("employes.dat");
+    int counterDelimStaff = counterCash.getTotalNumberEmployees(textFile);
+    std::string** tableFileText = counterCash.parsingString(textFile);
+
+    std::string entryPosition;
+    char choice;
+    std::vector<CounterCash*> ptrCounterCash;
+    do
+    {
+
+        std::cout << "Enter position: ";
+        std::cin >> entryPosition;
+
+        if(entryPosition == "director")
+        {
+            salaryCalculation(counterDelimStaff, entryPosition, counterCash, tableFileText, ptrCounterCash);
+        }
+        else if(entryPosition == "programmer")
+        {
+            salaryCalculation(counterDelimStaff, entryPosition, counterCash, tableFileText, ptrCounterCash);
+        }
+        else if(entryPosition == "assistant")
+        {
+            salaryCalculation(counterDelimStaff, entryPosition, counterCash, tableFileText, ptrCounterCash);
+        }
+        else
+        {
+            std::cout << "Unknown position " << std::endl;
+        }
+
+
+        std::cout << "Choose another position or finish (y/n)?: ";
+        std::cin >> choice;
+    }
+    while(choice == 'y');
+
+
+
+    return 0;
+}
---
+++ main_smartPtr.cpp
@@ -0,0 +1,297 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <memory>
+
+struct Employee
+{
+    std::string position;
+    std::string name;
+    std::string workTime;
+
+    Employee() : position(), name(), workTime()
+    {}
+
+    Employee(std::string Position, std::string Name, std::string WorkTime) :
+        position(Position),
+        name(Name),
+        workTime(WorkTime)
+    {}
+
+    void show()
+    {
+        std::cout << position << '\n';
+        std::cout << name << '\n';
+        std::cout << workTime << '\n';
+    }
+};
+
+
+class CounterCash
+{
+protected:
+    std::string nameEmp;
+    unsigned int workTimeEmp;
+    unsigned int salaryEmp;
+
+private:
+    unsigned int coeffDirector;
+    unsigned int coeffProgrammer;
+    unsigned int coeffAssistant;
+
+    unsigned int counterDelimStaff;
+
+    std::string fileText;
+    std::string clearStr;
+    std::vector<std::unique_ptr<Employee>> uniquePtrEmp;
+    std::unique_ptr<std::string[]> uniquePtrClearText;
+
+public:
+    CounterCash() : nameEmp(), workTimeEmp(0), salaryEmp(0), coeffDirector(0), coeffProgrammer(0), coeffAssistant(0), counterDelimStaff(0), fileText(), clearStr(), uniquePtrClearText(new std::string[3])
+    {}
+
+    CounterCash(unsigned int coeffDir, unsigned int coeffProg, unsigned int coeffAssist) :
+        nameEmp(),
+        workTimeEmp(0),
+        salaryEmp(0),
+        coeffDirector(coeffDir),
+        coeffProgrammer(coeffProg),
+        coeffAssistant(coeffAssist),
+        counterDelimStaff(0),
+        fileText(),
+        clearStr(),
+        uniquePtrClearText(new std::string[3])
+    {}
+
+
+    //Считываем текст из файлов без спец вимволов типа '\n' и считаем количество записей
+    std::string readFile(const char* filePath)
+    {
+        std::ifstream fileStream(filePath, std::ios::in);
+        if(!fileStream)
+        {
+            std::cout << "Error Open file" << std::endl;
+            exit(1);
+        }
+        while(fileStream)
+        {
+            if(getline(fileStream, fileText, '\n'))
+            {
+                clearStr += fileText;
+                ++counterDelimStaff;
+            }
+        }
+
+        return clearStr;
+    }
+
+
+    //Парсим строку заполняя структуру Employee, добавляя каждый экземпляр структуры Employee в вектор со смарт поинтерами
+    std::vector<std::unique_ptr<Employee>> parsingString(const std::string& clearStr)
+    {
+        unsigned int currentCursorPosition = 0;
+
+        for(unsigned int i = 0; i < counterDelimStaff; ++i)
+        {
+            for(unsigned int j = 0; j < 3; ++j)
+            {
+                while(clearStr[currentCursorPosition] != ';')
+                {
+                    uniquePtrClearText[j] = uniquePtrClearText[j] + clearStr[currentCursorPosition];
+                    ++currentCursorPosition;
+                }
+                ++currentCursorPosition;
+            }
+            uniquePtrEmp.emplace_back(new Employee(uniquePtrClearText[0], uniquePtrClearText[1], uniquePtrClearText[2]));
+            uniquePtrClearText[0].clear();
+            uniquePtrClearText[1].clear();
+            uniquePtrClearText[2].clear();
+        }
+
+        return std::move(uniquePtrEmp);
+    }
+
+
+    int crutchDir ()
+    {return coeffDirector;}
+
+
+    int crutchProg ()
+    {return coeffProgrammer;}
+
+
+    int crutchAssist ()
+    {return coeffAssistant;}
+
+
+    virtual void payRoll(CounterCash& counterCash)
+    {}
+
+
+    virtual void showSalary()
+    {}
+
+
+    virtual ~CounterCash()
+    {}
+};
+
+
+class Director : public CounterCash
+{
+public:
+    Director(const std::string& Name, const unsigned int WorkTime)
+    {
+        nameEmp = Name;
+        workTimeEmp = WorkTime;
+    }
+
+
+    void payRoll(CounterCash& counterCash)
+    {
+        unsigned int crutchCoeff = counterCash.crutchDir();
+        salaryEmp = workTimeEmp * crutchCoeff;
+    }
+
+
+    void showSalary()
+    {
+        std::string str = std::to_string(salaryEmp);
+        std::cout << nameEmp << "  " << str + "$" << std::endl;
+    }
+
+
+    ~Director()
+    {}
+};
+
+
+class Programmer : public CounterCash
+{
+public:
+    Programmer(const std::string& Name, const unsigned int WorkTime)
+    {
+        nameEmp = Name;
+        workTimeEmp = WorkTime;
+    }
+
+
+    void payRoll(CounterCash& counterCash)
+    {
+        unsigned int crutchCoeff = counterCash.crutchProg();
+        salaryEmp = workTimeEmp * crutchCoeff;
+    }
+
+
+    void showSalary()
+    {
+        std::string str = std::to_string(salaryEmp);
+        std::cout << nameEmp << "  " << str + "$" << std::endl;
+    }
+
+
+    ~Programmer()
+    {}
+};
+
+
+class Assistant : public CounterCash
+{
+public:
+    Assistant(const std::string& Name, const unsigned int WorkTime)
+    {
+        nameEmp = Name;
+        workTimeEmp = WorkTime;
+    }
+
+
+    void payRoll(CounterCash& counterCash)
+    {
+        unsigned int crutchCoeff = counterCash.crutchAssist();
+        salaryEmp = workTimeEmp * crutchCoeff;
+    }
+
+
+    void showSalary()
+    {
+        std::string str = std::to_string(salaryEmp);
+        std::cout << nameEmp << "  " << str + "$" << std::endl;
+    }
+
+
+    ~Assistant()
+    {}
+};
+
+void salaryCalculation(const std::string& EntryPos,
+                       const unsigned int sizeUniquePtrCounterCash,
+                       CounterCash& counterCash,
+                       std::vector<std::unique_ptr<Employee>>& UniquePtrEmployee,
+                       std::vector<std::unique_ptr<CounterCash>>& UniquePtrCounterCash)
+{
+    unsigned int index = 0;
+    unsigned int convertStringToIntPay = 0;
+
+    for(int i = 0; i < sizeUniquePtrCounterCash; ++i)
+    {
+        if(UniquePtrEmployee[i]->position == EntryPos)
+        {
+            std::istringstream(UniquePtrEmployee[i]->workTime) >> convertStringToIntPay;
+            UniquePtrCounterCash.emplace_back(new Director(UniquePtrEmployee[i]->name, convertStringToIntPay));
+            UniquePtrCounterCash[index]->payRoll(counterCash);
+            UniquePtrCounterCash[index]->showSalary();
+            ++index;
+        }
+    }
+    if(index != 0)
+    {
+        index = 0;
+        UniquePtrCounterCash.clear();
+    }
+}
+
+
+
+int main()
+{
+    CounterCash counterCash(500, 350, 200);
+    std::string fileTxt = counterCash.readFile("employes.dat");
+    std::vector<std::unique_ptr<Employee>> uniquePtrEmployee = counterCash.parsingString(fileTxt);
+    unsigned int size = uniquePtrEmployee.size();
+
+    std::string entryPosition;
+    char choice;
+    std::vector<std::unique_ptr<CounterCash>> uniquePtrCounterCash;
+    do
+    {
+        std::cout << "Enter position: ";
+        std::cin >> entryPosition;
+
+
+        if(entryPosition == "director")
+        {
+            salaryCalculation(entryPosition, size, counterCash, uniquePtrEmployee, uniquePtrCounterCash);
+        }
+        else if(entryPosition == "programmer")
+        {
+            salaryCalculation(entryPosition, size, counterCash, uniquePtrEmployee, uniquePtrCounterCash);
+        }
+        else if(entryPosition == "assistant")
+        {
+            salaryCalculation(entryPosition, size, counterCash, uniquePtrEmployee, uniquePtrCounterCash);
+        }
+        else
+        {
+            std::cout << "Unknown position " << std::endl;
+        }
+
+
+        std::cout << "Choose another position or finish (y/n)?: ";
+        std::cin >> choice;
+    }
+    while(choice == 'y');
+
+
+    return 0;
+}
